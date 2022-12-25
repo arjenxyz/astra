00f624f0c62eb7916f95a8259a3c4d78082bd48b
Arjen <96284993+italyanxyz@users.noreply.github.com>
2022-12-25 15:18:08 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,7 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Astra için Gizlilik Politikası</h1>
             <p className="text-sm text-white/30 mb-4">Son Güncelleme: 25/12/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Astra botu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
         <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
