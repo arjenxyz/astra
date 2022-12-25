e827205571fbc7306b842d2359169d767ac9b1de
Arjen <96284993+italyanxyz@users.noreply.github.com>
2022-12-25 15:28:43 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,8 +1,8 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
+      <p className="text-sm text-white/30 mb-1">Son güncelleme: 25/12/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
