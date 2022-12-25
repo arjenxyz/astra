bc4490f12bd52f3254f6e373b58589dd70f39ef7
Arjen <96284993+italyanxyz@users.noreply.github.com>
2022-12-25 16:29:28 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -34,75 +34,14 @@ export default function Footer() {
                             </Menu>
                         
                         </div>
-                        <div className="col-span-1">
-                            <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Links</p>
-                            <div>
-                                <Link href="https://bit.ly/3LYzaDe">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                        Vote Bot
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <a href="https://discord.gg/ZcDprCrFTn" target="_blank" className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    Support Server
-                                </a>
-                            </div>
-                            <div>
-                                <a href=" https://bit.ly/3PHDjyC" target="_blank" className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    Invite Bot
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-span-1">
-                            <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Social</p>
-                            <div>
-                                <Link href="https://discord.gg/ST89uArTdh">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-discord`} /> Disocrd 
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href="https://github.com/1umutda/MusicBot">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-github`} /> GitHub 
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href="https://youtube.com/UmutBayraktarYT">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-youtube`} /> YouTube 
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="col-span-1">
-                            <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Important</p>
-                            <div>
-                                <Link href="/tos">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                        Terms Of Service
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href="/privacy">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                        Privacy Policy
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
+                        
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <p className="text-white text-center sm:text-left text-opacity-50">
                             {new Date().getFullYear()} &copy; Astra Bot
                         </p>
                         <div className="hidden md:flex items-center justify-center">
                                 <p className={"text-xs text-green-400"}>
-                                    Bot Online
+                                    Astra Müzik Botu Çevrimiçi
                                 </p>
                         </div>
                         <p className="text-white text-center sm:text-right text-opacity-50">
