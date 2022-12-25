9b6ffe70e38c29f566d68aee76141cb2ecb49f1a
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2022-12-25 21:33:44 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,7 @@ Vote Astra</a>
                   <p className="text-3xl text-white font-semibold">En iyi müzik botu</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel bot Astra ile en güzel partileri vermeye hazır mısınız?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/1052258751681400902/1056616064668160040/Screenshot_1.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/1052258751681400902/1056640755285889085/Screenshot_2022-12-25-21-32-58-205.jpg" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
