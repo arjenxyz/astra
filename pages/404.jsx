23bc2b24dcee11a97853917b37fd7305ff0eb667
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2022-12-25 18:32:23 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,9 @@ const UnkownPage = () => {
     let messages = [
         'Yolunu mu kaybettin ufaklık?',
         'Kahretsin! Çıkmaz yola girdin..',
-        'İtalyan bu yolu kapattı. Harry!'
+        'Rotanı kaybetme evlat!',
+        'Sınır dışısın! Üsse geri dön asker!',
+        'İtalyan bu yolu kapattı Harry!'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Sayfa bulunamadı."} />
