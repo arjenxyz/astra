6da0f60a1a4130caa821f00dd719b53bc2499c74
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2022-12-25 16:59:56 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
 
                                 </div>
                             </Menu>
-                        
+                        </div>
                         </div>
                         
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
