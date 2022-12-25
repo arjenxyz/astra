b946865c21893843061154276e221445aa8c2972
Arjen <96284993+italyanxyz@users.noreply.github.com>
2022-12-25 13:17:48 +0300

    Update 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -1,8 +1,6 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
-    return <ErrorPage code={500} message={"Imm.. Görünüşe göre 500 Dahili Sunucu Hatası var. Lütfen daha sonra tekrar deneyiniz!
-
-"} />
+    return <ErrorPage code={500} message={"Imm.. Görünüşe göre 500 Dahili Sunucu Hatası var. Lütfen daha sonra tekrar deneyiniz!"} />
 }
 
 export default UnkownPage;
