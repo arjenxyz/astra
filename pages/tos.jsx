1684b94959720bec8634378ded4426052f14e247
Arjen <96284993+italyanxyz@users.noreply.github.com>
2022-12-25 15:33:07 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,15 +4,15 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
       <p className="text-sm text-white/30 mb-1">Son güncelleme: 25/12/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ </p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Diğer sunuculara zarar vermek için açık kaynak kodları kullanamazsınız.Her discord sunucusu Astra botu ücretsiz olarak kullanabilir, ücret karşılığında satılması veya pazarlanması kesinlikle yasaktır.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Diğer platformlar aracılığıyla açık kaynak kodlarını satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ Botu kullanırken diğer kullanıcılara zarar verecek şekilde troll amaçlı kullanamazsınız.</p>
 <div className="py-3"></div>
 <p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
 </div>
