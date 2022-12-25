25d652775bb86a15657948cceae2b4b9fe3fadf0
Arjen <96284993+italyanxyz@users.noreply.github.com>
2022-12-25 15:16:32 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,8 +2,8 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <h1 className="font-semibold text-3xl text-white/75">Astra için Gizlilik Politikası</h1>
+            <p className="text-sm text-white/30 mb-4">Son Güncelleme: 25/12/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
