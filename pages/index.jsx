343244242b40c9248b5e56c4af6065771e5aa2bc
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2022-12-26 13:24:13 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,7 +27,7 @@ export default function Index() {
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://bit.ly/3LYzaDe"}>
+  <Link href={"https://top.gg/bot/1048987730983534612/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote Astra</a>
   </Link>
