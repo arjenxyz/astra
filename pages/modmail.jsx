92aaace9372b7adf364c63003947d1ddc3a2955c
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2022-12-30 16:38:02 +0300

    Update modmail.jsx

--- pages/modmail.jsx
+++ pages/modmail.jsx
@@ -10,27 +10,23 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Bellatrix Bot
+          ModMail Bot
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-          Bellatrix bot ile kendi sunucunuza ritim katmaya ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi çalma listenizi oluşturun, bu keyfi en iyi seslerle yaşayın.
+          ModMail bot ile kendi sunucunuza daha hızlı bir destek verebeilirisiniz.
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://discord.com/oauth2/authorize?client_id=1048987730983534612&permissions=277028603968&scope=applications.commands%20bot"}>
+            <Link href={"https://discordapp.com/api/oauth2/authorize?client_id=1053636007825776680&permissions=8&scope=bot%20applications.commands"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Bellatrix'i Davet Et
+          ModMail'i Davet Et
               </a>
             </Link>
-  <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/1048987730983534612/vote"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Bellatrix</a>
-  </Link>
+ 
         </div>
 
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
@@ -45,10 +41,10 @@ Vote Bellatrix</a>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Bellatrix Bot" }} />
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"ModMail Bot" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Kendi duygularınızla kendinizi rahatlatabileceğiniz en iyi ortamı yaratmaya ne dersiniz?" }}/>
             <a href="https://discord.com/oauth2/authorize?client_id=1048987730983534612&permissions=277028603968&scope=applications.commands%20bot" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Bellatrix'i Davet Et
+              <i className="fab fa-discord mr-2" />Mod'i Davet Et
             </a>
           </div>
 
@@ -57,13 +53,13 @@ Vote Bellatrix</a>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimizasyon</p>
-                  <p className="text-gray-500 line-clamp-4">Bellatrix, sunucunuza özel hızlı ve özelleştirilebilir ayarlarla size müzik keyfini doyasıya yaşatır.</p>
+                  <p className="text-gray-500 line-clamp-4"> ModMail bot ile gizli ve güvenilir destekler içindir.</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Evrensel </p>
-                  <p className="text-gray-500 line-clamp-4">Çok platformlu müzik çalmayı destekleyen bir bot. (Spotify, Soundcloud, YouTube) | (Tüm telif hakları ve lisanslara uygun bir bottur.</p>
+                  <p className="text-gray-500 line-clamp-4"> DM üzerinden destek verir | (Tüm telif hakları ve lisanslara uygun bir bottur.</p>
                 </div>
               </div>
               <div>
@@ -86,11 +82,11 @@ Vote Bellatrix</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://cdn.discordapp.com/attachments/1053609617839628368/1057987357246234685/Screenshot_2022-12-29-14-43-30-966.jpg"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">En iyi müzik botu</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel bot Bellatrix ile en güzel partileri vermeye hazır mısınız?</p>
+                  <img src="https://media.discordapp.net/attachments/1058356484670505047/1058376834871480421/Screenshot_1.png?width=222&height=162"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <p className="text-3xl text-white font-semibold">ModMail Bot</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">İyi ve gelişmiş destek botu</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/1053609617839628368/1057987357246234685/Screenshot_2022-12-29-14-43-30-966.jpg" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://media.discordapp.net/attachments/1058356484670505047/1058376834871480421/Screenshot_1.png?width=222&height=162" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
