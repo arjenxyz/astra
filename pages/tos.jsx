030ae0920f60120d8d6d63b05d7de5371714e560
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2023-01-07 14:10:32 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,9 +4,9 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Matrix Bot Terms of Service</h2>
       <p className="text-sm text-white/30 mb-1">Last update: 25/01/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Matrix bot for free, it is strictly forbidden to sell or market it for a fee. </p>
+<p>⊳ Every discord server can use the Matrix bot for free, it is strictly forbidden to sell or market it for a fee.<b> <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
 <div className="py-3"></div>
-<p>⊳ </p>
+<p>⊳ When adding the Matrix bot to your discord server, you need to define the necessary permissions.</p>
 <div className="py-3"></div>
 <p>⊳ </p>
 <div className="py-3"></div>
