f1a1217c34d817db10cf0cbeab61190db9c6ae13
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2023-01-07 14:07:12 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,8 +1,8 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
-      <p className="text-sm text-white/30 mb-1">Son güncelleme: 25/01/2022</p>
+     <h2 className="mt-4 text-white font-bold text-3xl">Matrix Bot Terms of Service</h2>
+      <p className="text-sm text-white/30 mb-1">Last update: 25/01/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ Every discord server can use the Matrix bot for free, it is strictly forbidden to sell or market it for a fee. </p>
 <div className="py-3"></div>
