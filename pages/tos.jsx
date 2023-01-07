da29457b91597a732dee4423010e6b134a444b91
arjenxyz <121389146+arjenxyz@users.noreply.github.com>
2023-01-07 14:05:31 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,19 +2,19 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
-      <p className="text-sm text-white/30 mb-1">Son güncelleme: 25/12/2022</p>
+      <p className="text-sm text-white/30 mb-1">Son güncelleme: 25/01/2022</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Kodlamalar Github Sitemizde Yayınlanmıştır.</p>
+<p>⊳ Every discord server can use the Matrix bot for free, it is strictly forbidden to sell or market it for a fee. </p>
 <div className="py-3"></div>
-<p>⊳ Diğer sunuculara zarar vermek için açık kaynak kodları kullanamazsınız.Her discord sunucusu Astra botu ücretsiz olarak kullanabilir, ücret karşılığında satılması veya pazarlanması kesinlikle yasaktır.</p>
+<p>⊳ </p>
 <div className="py-3"></div>
-<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
+<p>⊳ </p>
 <div className="py-3"></div>
-<p>⊳ Diğer platformlar aracılığıyla açık kaynak kodlarını satamaz veya pazarlayamazsınız.</p>
+<p>⊳ </p>
 <div className="py-3"></div>
-<p>⊳ Botu kullanırken diğer kullanıcılara zarar verecek şekilde troll amaçlı kullanamazsınız.</p>
+<p>⊳ </p>
 <div className="py-3"></div>
-<p>⊳ Belirli discord bot komutlarını kullanmak için <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> botuna oy vermeniz gerekmektedir.</p>
+<p>⊳ </p>
 </div>
     </div>
   );
